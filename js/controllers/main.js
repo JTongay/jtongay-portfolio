961c1a477903ac3da2f14cd3dd781e8eb24f6b9a
Joseph Tongay <dirk.tongay@gmail.com>
2016-12-26 18:16:25 -0600

    everything should be single page now

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -12,24 +12,31 @@ app.controller('main', ['$scope','Image','$state', function($scope, Image, $stat
       id: 1,
       url: 'pokemonstay',
       title: "Pokemon Stay!",
-      image: "images/pokestay1.png"
+      image: "images/pokemonstay/pokestay1.png"
     },
     {
       id:2,
       url: 'digitalrolodex',
       title: "Digital Rolodex",
-      image: "images/digidex1.png"
+      image: "images/digidex/digidex1.png"
     },
     {
       id:3,
       url: 'railsreddit',
       title: "Rails Reddit Clone",
-      image: "images/railsreddit.png"
+      image: "images/railsreddit/railsreddit.png"
     },
     {
       id:4,
       url: 'raspi-tv',
       title: "Raspi Smart TV",
+      image: "images/raspi/raspi.png"
+    }
+  ]
+
+  $scope.view.raspi = [
+    {
+      id: 1,
       image: "images/raspi.png"
     }
   ]
